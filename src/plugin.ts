b3f8d64da4dfd582f00350a63b3e3ef5a2282bbb
Nazar <113494366+NazCodeland@users.noreply.github.com>
2024-09-15 18:11:28 -0400

    refactor: fix errors to make biome work

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,13 +1,13 @@
-if (figma.editorType === "figma") {
+if (figma.editorType === 'figma') {
   figma.showUI(__html__, {
     themeColors: true,
     width: 244 + 8,
     height: 1124,
-    title: "Viewport Screenshot",
+    title: 'Viewport Screenshot',
   });
 
   figma.ui.onmessage = (msg) => {
-    if (msg.type === "data-viewport") {
+    if (msg.type === 'data-viewport') {
       const MAX_IMAGE_HEIGHT = 4096;
       const options = {
         url: msg.options.url,
@@ -36,9 +36,9 @@ if (figma.editorType === "figma") {
               frame.resize(width, height);
               frame.fills = [
                 {
-                  type: "IMAGE",
+                  type: 'IMAGE',
                   imageHash: pngImage.hash,
-                  scaleMode: "FILL",
+                  scaleMode: 'FILL',
                 },
               ];
 
@@ -56,26 +56,26 @@ if (figma.editorType === "figma") {
 
             // Create a new frame to contain all the individual frames
             const containerFrame = figma.createFrame();
-            console.log("options.url", options.url);
-            containerFrame.name = options.url.split("https://")[1];
+            console.log('options.url', options.url);
+            containerFrame.name = options.url.split('https://')[1];
             containerFrame.resize(totalWidth, maxHeight);
-            containerFrame.layoutMode = "HORIZONTAL";
+            containerFrame.layoutMode = 'HORIZONTAL';
             containerFrame.verticalPadding = 0;
             containerFrame.horizontalPadding = 32;
-            containerFrame.primaryAxisSizingMode = "AUTO"; // Automatically size based on children
-            containerFrame.counterAxisSizingMode = "AUTO"; // Automatically size based on children
+            containerFrame.primaryAxisSizingMode = 'AUTO'; // Automatically size based on children
+            containerFrame.counterAxisSizingMode = 'AUTO'; // Automatically size based on children
             containerFrame.itemSpacing = 8; // Space between items
             // Add all frames to the container frame
             for (const frame of frames) {
               containerFrame.appendChild(frame);
             }
           } catch (error) {
-            console.log("error", error);
+            console.log('error', error);
           }
         })
         .catch((error) => {
-          console.log("error", error);
-          figma.notify("Failed to load image. Please check the URL.");
+          console.log('error', error);
+          figma.notify('Failed to load image. Please check the URL.');
           // figma.closePlugin();
         });
     }
