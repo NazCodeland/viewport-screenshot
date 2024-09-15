514dc6a790d885d2976bba7a79c0384bdddd10e7
Nazar <113494366+NazCodeland@users.noreply.github.com>
2024-09-14 23:47:14 -0400

    refactor: remove eslint and prettier in-favor of biome

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,3 @@
-export interface Dimension {
-  deviceType: string;
-  deviceName: string;
-  width: number;
-  height: number;
-  zoom: number;
-}
-
 export type Prettify<T> = {
   [K in keyof T]: T[K];
 };
